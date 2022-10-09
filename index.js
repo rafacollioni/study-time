b1d610eff2b081727448d6b8a460a3c91b2d6536
rafacollioni <rafa.collioni@gmail.com>
2022-10-09 20:31:19 -0300

    botões horas, minutos e segundos ok

--- index.js
+++ index.js
@@ -28,6 +28,35 @@ function settingTimer(e) {
 
         case 'minutes-up':
 
+            timerMinutes.innerHTML++
+            if (timerMinutes.innerHTML < 10){
+                timerMinutes.innerHTML = "0" + timerMinutes.innerHTML
+            }else if (timerMinutes.innerHTML > 59){
+                timerMinutes.innerHTML = "00"
+                timerHours.innerHTML < 10 ? timerHours.innerHTML = "0" + (Number(timerHours.innerHTML) + 1) : timerHours.innerHTML++
+            }
+            break
+
+        case 'seconds-up':
+
+            timerSeconds.innerHTML++
+            if (timerSeconds.innerHTML < 10){
+                timerSeconds.innerHTML = "0" + timerSeconds.innerHTML
+            }else if (timerSeconds.innerHTML > 59){
+                timerSeconds.innerHTML = "00"
+                timerMinutes.innerHTML < 10 ? timerMinutes.innerHTML = "0" + (Number(timerMinutes.innerHTML) + 1) : timerMinutes.innerHTML++
+            }
+            break
+        
+        case 'hours-down':
+            timerHours.innerText === "00" ? timerHours.innerHTML = 24 : timerHours.innerHTML--
+
+
+            if (timerHours.innerHTML < 10) {
+                timerHours.innerHTML = "0" + timerHours.innerHTML
+            } else 
+            break
+            
 
 
 

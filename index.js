91b9a854ec6dd717f5b09a0f2de4dc0dfc7fa599
rafacollioni <rafa.collioni@gmail.com>
2022-10-12 23:10:04 -0300

    TA FUNCIONANDO CARALHO

--- index.js
+++ index.js
@@ -12,63 +12,80 @@ function settingTimer(e) {
 
     // Pegando o Botão pelo Evento do Click, e usando SwitchCase para comparar com a classe
 
-    let pressedBtn = e.target
-
+    let pressedBtn = e.target;
+    // Como os botões tem duas classes, a classe na posição 1 do objeto é a que indica a operação que deve ser feita
     switch (pressedBtn.classList[1]) {
 
         case 'hours-up':
 
-            timerHours.innerHTML++
-            if (timerHours.innerHTML < 10) {
-                timerHours.innerHTML = "0" + timerHours.innerHTML
-            } else if (timerHours.innerHTML > 24) {
-                timerHours.innerHTML = "00"
-            }
+            changeHours(timerHours, "plus");
             break
 
         case 'minutes-up':
 
-            timerMinutes.innerHTML++
-            if (timerMinutes.innerHTML < 10){
-                timerMinutes.innerHTML = "0" + timerMinutes.innerHTML
-            }else if (timerMinutes.innerHTML > 59){
-                timerMinutes.innerHTML = "00"
-                timerHours.innerHTML < 10 ? timerHours.innerHTML = "0" + (Number(timerHours.innerHTML) + 1) : timerHours.innerHTML++
-            }
-            break
+            changeMinutesOrSeconds(timerMinutes, "plus", timerHours);
+            break;
 
         case 'seconds-up':
 
-            timerSeconds.innerHTML++
-            if (timerSeconds.innerHTML < 10){
-                timerSeconds.innerHTML = "0" + timerSeconds.innerHTML
-            }else if (timerSeconds.innerHTML > 59){
-                timerSeconds.innerHTML = "00"
-                timerMinutes.innerHTML < 10 ? timerMinutes.innerHTML = "0" + (Number(timerMinutes.innerHTML) + 1) : timerMinutes.innerHTML++
-            }
-            break
-        
+            changeMinutesOrSeconds(timerSeconds, "plus", timerMinutes);
+            break;
+
         case 'hours-down':
-            timerHours.innerText === "00" ? timerHours.innerHTML = 24 : timerHours.innerHTML--
+
+            changeHours(timerHours, "minus");
+            break;
+
+        case 'minutes-down':
+
+            changeMinutesOrSeconds(timerMinutes, "minus", timerHours);
+            break;
+
+        case 'seconds-down':
+
+            changeMinutesOrSeconds(timerSeconds, "minus", timerMinutes);;
+            break;
+
+    }
+
+}
+
+function startTimer() {
+    const intervalSeconds = 1000;
+    const intervalMinutes = intervalSeconds * 62;
+    const intervalHours = intervalMinutes * 60;
+
+    // Função dos Segundos
+
+    if (timerHours.innerHTML == "00" && timerMinutes.innerHTML == "00" && timerSeconds.innerHTML == "00") {
+        return "erro"
+    } else {
+        setInterval(function () {
+            timerSeconds.innerHTML--;
+            addZeros(timerSeconds)
+            if (timerSeconds.innerHTML == "0-1") {
+                timerSeconds.innerHTML = "60"
+                timerMinutes.innerHTML--
+            }
+            if (timerMinutes.innerHTML == "-1"){
+                timerMinutes.innerHTML = "59"
+                addZeros(timerHours);
+                timerHours.innerHTML--
+            }
+        }, intervalSeconds);
+
+    // setInterval(function () {
+    //         addZeros(timerMinutes);
+    //             if(timerMinutes.innerText == "-1") {
+    //                 timerMinutes.innerHTML = "60";
+    //                 timerHours--
+    //             }
 
 
-            if (timerHours.innerHTML < 10) {
-                timerHours.innerHTML = "0" + timerHours.innerHTML
-            } else 
-            break
-            
 
-
-
-
-
-
-
-
-
-
-
-
+    // }, intervalMinutes)
+    
+    
 
 
 
@@ -77,4 +94,70 @@ function settingTimer(e) {
 
     }
 
+}
+
+
+function addZeros(element) {
+
+    // Se o número for menor que "10", a função adiciona um "0" antes do número;
+
+    if (element.innerHTML < 10) {
+        element.innerHTML = "0" + element.innerHTML;
+    }
+}
+
+function changeHours(hourNumber, plusOrMinus) {
+
+    // Dependendo do botão pressionado, essa função vai ser chamada, trazendo o elemento a ser mudado, e a operação (+ ou -);
+
+    // Aumentar o Número de Horas:
+
+    if (plusOrMinus === "plus") {
+        hourNumber.innerHTML++;
+
+        // É chamada a função que adiciona um "0"
+
+        addZeros(hourNumber);
+
+        // Se o número de horas passar de 24, simplesmente é zerado o parametro das horas;
+
+        if (hourNumber.innerHTML > 24) {
+            hourNumber.innerHTML = "00";
+
+            // Diminuir o Número de Horas:
+
+        }
+
+    } else {
+
+        // Se o número de horas for 0, automaticamente é colocado em 24;
+
+        if (hourNumber.innerHTML == 0) {
+            hourNumber.innerHTML = 24;
+        } else {
+            hourNumber.innerHTML--;
+            addZeros(hourNumber);
+        }
+    }
+}
+
+
+function changeMinutesOrSeconds(numberToChange, plusOrMinus, secondaryNumberToChange) {
+
+    // A única diferença aqui é que é necessário mudar dois valores, o do botão pressionado e o valor seguinte a ele.
+
+    if (plusOrMinus === "plus") {
+        numberToChange.innerHTML++;
+        if (numberToChange.innerHTML < 10) {
+            numberToChange.innerHTML = "0" + numberToChange.innerHTML;
+        } else if (numberToChange.innerHTML > 59) {
+            numberToChange.innerHTML = "00";
+            secondaryNumberToChange.innerHTML < 10 ? secondaryNumberToChange.innerHTML = "0" + (Number(secondaryNumberToChange.innerHTML) + 1) : secondaryNumberToChange.innerHTML++;
+        }
+    } else {
+        numberToChange.innerText === "00" ? numberToChange.innerHTML = 60 : numberToChange.innerHTML--;
+        if (numberToChange.innerHTML < 10) {
+            numberToChange.innerHTML = "0" + numberToChange.innerHTML;
+        }
+    }
 }
